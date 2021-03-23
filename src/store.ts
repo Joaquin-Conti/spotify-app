a052baed52a96192aeab7ff580148e0726492c63
Joaquin Conti <joacoconti@hotmail.com>
2021-03-23 14:01:20 -0300

    fixed version

--- src/store.ts
+++ src/store.ts
@@ -10,9 +10,9 @@ const sagaMiddleware = createSagaMiddleware()
 const saveToLocalStorage = (state: any) => {
   try {
     const serializedState = JSON.stringify(state)
-    if (localStorage.getItem("app-version") !== '4') {
+    if (localStorage.getItem("app-version") !== '5') {
       localStorage.clear();
-      localStorage.setItem("app-version", "4");
+      localStorage.setItem("app-version", "5");
     }
     localStorage.setItem('state', serializedState)
   } catch (err) {
